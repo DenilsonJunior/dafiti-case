4005502fa560db10be711924f8bc12783527edd1
Devnilson <dehjr1990@gmail.com>
2024-06-25 21:50:46 -0300

    [FACTURE - Inserindo box de texto]

--- src/components/guide/Guide.jsx
+++ src/components/guide/Guide.jsx
@@ -4,9 +4,27 @@ function Guide() {
     return (
         <div className='min-conteudo'>
             <SecGuide>
-                <img className='img img1' src="./assets/img/img1.jpg" alt="" />
-                <img className='img img2' src="./assets/img/img2.jpg" alt="" />
-                <img className='img img3' src="./assets/img/img3.jpg" alt="" />
+                <div className='card card1'>
+                    <img className='img' src="./assets/img/img1.jpg" alt="" />
+                    <div className='boxText'>
+                        <p className='tl'>Where does it come from</p>
+                        <p className='tm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.</p>
+                    </div>
+                </div>
+                <div className='card card2'>
+                    <img className='img' src="./assets/img/img2.jpg" alt="" />
+                    <div className='boxText'>
+                        <p className='tl'>Where does it come from</p>
+                        <p className='tm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.</p>
+                    </div>
+                </div>
+                <div className='card card3'>
+                    <img className='img' src="./assets/img/img3.jpg" alt="" />
+                    <div className='boxText'>
+                        <p className='tl'>Where does it come from</p>
+                        <p className='tm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.</p>
+                    </div>
+                </div>
             </SecGuide>
         </div>
     )
--- src/components/guide/styles.js
+++ src/components/guide/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const SecGuide = styled.section`
-    margin: 1rem 0;
+    margin: 1rem 0 5rem;
 
     display: grid;
     gap: 1rem;
@@ -9,24 +9,53 @@ export const SecGuide = styled.section`
         "a a c c"
         "b b c c";
 
-    @media (max-width: 768px) {
+    @media (max-width: 728px) {
         grid-template-areas: 
             "a a"
             "b b"
             "c c";
     }
 
-    .img {
+    .card {
         width: 100%;
+        height: auto;
+        position: relative;
+        overflow: hidden;
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        .boxText {
+            backdrop-filter: blur(2px);
+            transform: translateY(5rem);
+            background-color: #0009;
+            position: absolute;
+            bottom: 0;
+            padding: .5rem;
+            transition-duration: .4s;
+            width: 100%;
+            p {
+                color: white;
+            }
+            .tm {
+                font-size: .7rem;
+            }
+        }
+        &:hover {
+            .boxText {
+                transform: translate(0);
+            }
+        }
     }
 
-    .img1 {
+    .card1 {
         grid-area: a;
     }
-    .img2 {
+    .card2 {
         grid-area: b;
     }
-    .img3 {
+    .card3 {
         grid-area: c;
     }
 `
